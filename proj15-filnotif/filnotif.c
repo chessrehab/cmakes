3a92b180f7095ed5abdee4f8a08d5b362eb2af18
marty <martin.svec@gmail.com>
2020-04-23 23:36:33 +0200

    updating...

---
+++ proj15-filnotif/filnotif.c
@@ -0,0 +1,14 @@
+#include "start01.h"
+
+#define MAX_BUF 100
+#define PATH01 "/home/marty/cmakes/proj15-filnotif/data/test01.dat"
+#define PATH02 "/home/marty/cmakes/proj15-filnotif/data/test02.dat"
+#define PATH03 "/home/marty/cmakes/proj15-filnotif/data/test03.dat"
+
+int main() {
+    
+}
+
+
+
+
