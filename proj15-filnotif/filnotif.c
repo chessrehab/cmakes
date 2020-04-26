c83d201b3abb54bba396b3d4b76cc157fb6ba8e3
martyx@martyx-HP <martyx@martyx-HP.sk>
2020-04-26 23:11:41 +0200

    updating...

--- proj15-filnotif/filnotif.c
+++ proj15-filnotif/filnotif.c
@@ -1,12 +1,32 @@
 #include "start01.h"
+#include <sys/inotify.h>
 
 #define MAX_BUF 100
-#define PATH01 "/home/marty/cmakes/proj15-filnotif/data/test01.dat"
-#define PATH02 "/home/marty/cmakes/proj15-filnotif/data/test02.dat"
-#define PATH03 "/home/marty/cmakes/proj15-filnotif/data/test03.dat"
+#define PATH01 "/home/martyx/cmakes/proj15-filnotif/data/test01.dat"
+#define PATH02 "/home/martyx/cmakes/proj15-filnotif/data/test02.dat"
+#define PATH03 "/home/martyx/cmakes/proj15-filnotif/data/test03.dat"
 
 int main() {
-    
+	int fd1=inotify_init();
+	int wd1=inotify_add_watch(fd1,PATH01,IN_ALL_EVENTS);
+	char  buf[4096]
+		__attribute__ ((aligned(__alignof__(struct inotify_event))));
+	char *p;
+	struct inotify_event *events;
+	while(1) {
+		int i= read(fd1,buf,sizeof(buf));
+		if (i>0) printf("\nbuffer bytes read:%d\n",i);
+		for(p=buf;p<buf+i;p+=sizeof(struct inotify_event)+events->len) {
+			events=(struct inotify_event *) buf;
+			printf("\nstruct inotify_events.wd=%d\n",events->wd);
+			printf("\nstruct inotify_events.mask=%o\n",(int) events->mask);
+			printf("\nstruct inotify_events.cookie=%d\n",events->cookie);
+			printf("\nstruct inotify_events.name=%s\n", events->name);
+			printf("\nstruct inotify_events.len=%d\n",events->len);
+			
+		}
+	}
+
 }
 
 
--- proj16-signals/signals.c
+++ proj16-signals/signals.c
@@ -1,12 +1,18 @@
 #include "start01.h"
-
+#include <signal.h>
 #define MAX_BUF 100
-#define PATH01 "/home/marty/cmakes/proj16-signals/data/test01.dat"
-#define PATH02 "/home/marty/cmakes/proj16-signals/data/test02.dat"
-#define PATH03 "/home/marty/cmakes/proj16-signals/data/test03.dat"
+#define PATH01 "/home/martyx/cmakes/proj16-signals/data/test01.dat"
+#define PATH02 "/home/martyx/cmakes/proj16-signals/data/test02.dat"
+#define PATH03 "/home/martyx/cmakes/proj16-signals/data/test03.dat"
+
+void handler1(int s) {
+	printf("\nsignal recieved:%o\nsignal desc:%s\n",s,strsignal(s));
+	psignal(s,"msg desc disgnal:");
+}
 
 int main() {
-    
+	void (*fc_pointer)(int)=signal(2,handler1);
+    	while(1);
 
 }
 
