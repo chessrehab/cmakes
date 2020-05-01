6eb9d48655f3492adee703e52eabc49772a5a84d
martyx@martyx-HP <martyx@martyx-HP.sk>
2020-05-02 00:43:41 +0200

    updating...

--- proj16-signals/signals.c
+++ proj16-signals/signals.c
@@ -1,19 +1,26 @@
-
+//#define _GNU_SOURCE
 #include "start01.h"
 #include <signal.h>
 #define MAX_BUF 100
 #define PATH01 "/home/martyx/cmakes/proj16-signals/data/test01.dat"
 #define PATH02 "/home/martyx/cmakes/proj16-signals/data/test02.dat"
 #define PATH03 "/home/martyx/cmakes/proj16-signals/data/test03.dat"
-//_POSIX_C_SOURCE >=1 || _XOPEN_SOURCE || _POSIX_SOURCE
+//#define _POSIX_C_SOURCE >=199309L
+
 void handler1(int s) {
 	printf("\nsignal recieved:%o\nsignal desc:%s\n",s,strsignal(s));
 	psignal(s,"msg desc disgnal:");
 }
+static void handler_info(int sig,siginfo_t *siginfo,void *ucontext) {
+	printf("\n signal recieved:%o(%s)\n",sig,strsignal(sig));
+	printf ("\n signal info:si_signo:%d,si_code:%d\n",siginfo->si_signo,siginfo->si_code);
+	printf ("\n \n");
+}
 
 int main() {
-	void (*fc_pointer)(int)=signal(2,handler1);
-	sigset_t  myset;
+//	void (*fc_pointer)(int)=signal(2,handler1);
+	sigset_t  myset,myset2,fullmask,prevmask;
+
 	if(sigemptyset(&myset)==-1) errExit("error:emptyset creation");
 	if(sigaddset(&myset,SIGKILL)==-1) errExit("error:addset");
 	if(sigaddset(&myset,SIGHUP)==-1) errExit("addset");
@@ -22,11 +29,31 @@ int main() {
 	if(sigdelset(&myset,SIGKILL)==-1) errExit("delset");
 	
 	(sigismember(&myset,SIGKILL)==1) ? printf("\nkillsignal nr. 9 is member of signalset") : printf("\nkillsignal nr. 9 isnt a member of signalset");
+	if(sigaddset(&myset,SIGKILL)==-1) errExit("addset");
+	sigfillset(&fullmask);
+	sigdelset(&fullmask,3);
+	sigprocmask(SIG_SETMASK,&fullmask,&prevmask);
 	
-	if(sigprocmask(SIG_SETMASK,&myset,NULL)==-1) errExit("sigprocmask");
+	if(sigemptyset(&myset2)==-1) errExit("sigfillset");
+	if(sigdelset(&myset2,SIGKILL)==-1) errExit("sigdelset");
 	
-    	while(1);
+	struct sigaction act;
+	act.sa_sigaction=handler_info;
+	sigfillset(&act.sa_mask);
+	act.sa_flags= SA_SIGINFO ;
+	
+	for(int i=1;i<NSIG;i++) {
+		sigaction(i,&act,NULL);	
+//		printf("\nnumber:%d(%s)",i,strsignal(i));
+	}
+	
+	
+		
 
+	printf("\nPID:%d",getpid());
+	printf("\nx");
+    	sleep(100);
+	sigprocmask(SIG_SETMASK,&prevmask,NULL);
 }
 
 
---
+++ proj17-timers/timer01.c
@@ -0,0 +1,15 @@
+#include "start01.h"
+
+#define MAX_BUF 100
+#define PATH01 "/home/martyx/cmakes/proj17-timers/data/test01.dat"
+#define PATH02 "/home/martyx/cmakes/proj17-timers/data/test02.dat"
+#define PATH03 "/home/martyx/cmakes/proj17-timers/data/test03.dat"
+
+int main() {
+    
+
+}
+
+
+
+
