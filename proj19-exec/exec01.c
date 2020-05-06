104c3a9b70bebbb159ad4629446aeb7f25d8aa8f
martyx@martyx-HP <martyx@martyx-HP.sk>
2020-05-06 23:47:42 +0200

    updating...

--- proj19-exec/exec01.c
+++ proj19-exec/exec01.c
@@ -1,6 +1,9 @@
+#define _GNU_SOURCE
 #include "start01.h"
 #include <unistd.h>
 #include <sys/wait.h>
+#include <sched.h>
+#include <stdio.h>
 
 #define MAX_BUF 100
 #define PATH01 "/home/martyx/cmakes/proj19-exec/data/test01.dat"
@@ -11,11 +14,16 @@ void ex1() {
 	printf("exiting parent...");
 
 }
+int  clonned(void *vd) {
+	printf("\nentered the clonned func");
+	printf("\n message from other galaxy:%s\n",(char *) vd);
+	exit(0);
+}
 
 
 int main(int argc, char *argv[]) {
 	atexit(ex1);
-	int fk,stat;
+	int fk,stat,stat2;
 	switch (fk=fork()) {
 		case -1:
 			errExit("forky error...");
@@ -27,7 +35,15 @@ int main(int argc, char *argv[]) {
 			waitpid(fk,&stat,0);
 			printf("\nchild did its job and exited with a code:%d\n",stat);
 			system("ps -al| grep exec01");
-			system("gitsync");
+//			system("gitsync");
+			void *stck=malloc(64000);
+			void *stck_top=stck+64000;
+			char msg[]="ahojte";
+			pid_t new_cloned_pid= clone(clonned,stck_top,CLONE_VM|CLONE_SIGHAND|SIGUSR1,&msg);
+			pid_t i=waitpid(new_cloned_pid,&stat2,__WCLONE);
+			printf("\n%d\n",i);
+			printf("\n%d joined!\n",i);
+			fflush(stdout);
 			exit(0);
 	}
 }
