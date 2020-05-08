d6f8f0b0415861004d4486b792fdb0dbf30b0108
marty <martin.svec@gmail.com>
2020-05-08 22:34:30 +0200

    updating...

---
+++ proj20-threads/thread01.c
@@ -0,0 +1,14 @@
+#include "start01.h"
+
+#define MAX_BUF 100
+#define PATH01 "/home/marty/cmakes/proj20-threads/data/test01.dat"
+#define PATH02 "/home/marty/cmakes/proj20-threads/data/test02.dat"
+#define PATH03 "/home/marty/cmakes/proj20-threads/data/test03.dat"
+
+int main() {
+	    
+}
+
+
+
+
