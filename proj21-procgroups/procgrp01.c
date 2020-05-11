aa507f71d2b17580c62e1923c2ebfb2e26d26630
marty <martin.svec@gmail.com>
2020-05-11 23:37:23 +0200

    updating...

--- proj21-procgroups/procgrp01.c
+++ proj21-procgroups/procgrp01.c
@@ -1,4 +1,4 @@
-
+#include <unistd.h>
 #include "start01.h"
 
 #define MAX_BUF 100
@@ -8,7 +8,24 @@
 
 
 int main() {
-	
+	setbuf(stdout,NULL);
+	printf("\nproc id:%d\n",getpid());
+	printf("\nproc group id:%d\n", getpgrp());	
+	switch(fork()) {
+		case -1:
+			errExit("fork");
+		case 0:
+			printf("\nchild proc id:%d",getpid());
+			printf("\nchild proc group id:%d\n", getpgrp());	
+			setpgid(0,0);
+			printf("\n2 child proc id:%d",getpid());
+			printf("\n2 child proc group id:%d\n", getpgrp());	
+			exit(0);
+
+		default:
+			printf("\nparent proc id:%d",getpid());
+			printf("\nparent proc group id:%d\n", getpgrp());	
+	}
 }
 
 
