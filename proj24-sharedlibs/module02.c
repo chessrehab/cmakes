6bd2858b4dcfb0a6efc74967137ddc94868df061
martyx@martyx-HP <martyx@martyx-HP.sk>
2020-05-20 22:08:27 +0200

    updating...

---
+++ proj24-sharedlibs/module02.c
@@ -0,0 +1,7 @@
+#include "header01.h"
+
+
+int module02_speaks() {
+	printf("\nmodule02 spoke\n");
+	return 0;
+}
---
+++ proj24-sharedlibs/starter01.c
@@ -0,0 +1,13 @@
+#include "header01.h"
+
+int main(int argc, char *argv[]) {
+	printf("\nstarting....\n");
+	module01_speaks();
+	module02_speaks();
+	module03_speaks();
+
+	printf("\nshutting down...");
+	return 0;
+
+	
+}
