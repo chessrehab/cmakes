d7d4e1f0fabdec7fc707cc1b99f796e7b4a1978a
mx <martin.svec@gmail.com>
2021-01-04 00:52:43 +0100

    updating...

--- asm/01/prog01.asm
+++ asm/01/prog01.asm
@@ -11,12 +11,13 @@
 
 
 section .data                               ;initialized global variables area
-	otpt db "Hello from ASM\n",10               ;definition of a string, with pointer otpt pointing to first byte
-	otpt_len equ $-otpt                     ;definition of a constant for string length calculated from the memory pointer minus first byte of string
-	val1 dud  128
-	val2 dd  128
+	otpt: db "Hello from ASM",10               ;definition of a string, with pointer otpt pointing to first byte
+	otpt_len: equ $-otpt                     ;definition of a constant for string length calculated from the memory pointer minus first byte of string
+	val1: dd  128
+	val2: dd  4
 
-section .bss                                ;uninitialized global variables area
+;section .bss                                ;uninitialized global variables area
+	val3 dd
 
 section .text                               ;text of code area
 	global _start                           ;entry point defining label entry point of code
@@ -24,24 +25,33 @@ section .text                               ;text of code area
 _start:                                     ;label entry point
 	nop                                     ;no operation, point for debugger to stop
 ;.............................................................................................sandbox
-	mov eax,0FFFFFFFFh
+	mov al,06h
+	add dword [val2], eax
+	mov eax,dword [val2]
+	push ax
+	push eax
+	push word [val2]
+	pop dx
+	pop dword [val3]
+	pushf
+	popf 
+	push 00400h
+	popfd
+	mov eax,6
+	mov ecx,7
+	mov edx,8
+	mov ebx,9
+	mov ebp,10
+	mov esi,11
+	mov edi,12
+	pushad
+	popad
 
-	mov ecx,val1
-	mov ecx,0
-	movzx ecx,BYTE [val1]
-	neg BYTE [val1]
-	neg BYTE [val1]
-	inc ecx
-	mov ecx, 8
-;	mov [val1],ecx
-	add BYTE [val1],1
-	sub [val1],DWORD 9
-	dec DWORD [val1]
+
+
+
+
 	nop
-	add DWORD [val1], 2
-	mov ecx,DWORD [val1]
-	sub ecx,6
-;	inc [ecx]
 ;..............................................................................................sandbox
 	jmp .fnct
 .fnct:
@@ -50,16 +60,16 @@ _start:                                     ;label entry point
 	mov ecx,otpt                            ;move a pointer to first char byte for stdout into ecx register
 	mov edx,otpt_len                        ;move a string length variable to edx register
 	int 080h                                ;call linux system call (software interrupt)
-	jmp _exit
+	jmp .exit
 
 
 	nop                                     ;no operation, point for debugger to stop
 
-_exit:
+.exit:
 	mov eax,1                               ;move code for exit(1) linux system call into eax register
 	mov ebx,0                               ;move program return value to register ebx
 	int 80h                                 ;call linux api system call(software interrupt)
-
+	nop
  
 
 
