417c7722ed106ae99c8d76f577c99fbc6b2db049
mx <martin.svec@gmail.com>
2021-01-06 13:40:35 +0100

    updating...

--- asm/05/hxdump2.asm
+++ asm/05/hxdump2.asm
@@ -16,18 +16,17 @@ section .data
 	       db 0x2E,0x61,0x62,0x63,0x64,0x65,0x66,0x67,0x68,0x69,0x6A,0x6B,0x6C,0x6D,0x6E,0x6F
 	       db 0x70,0x71,0x72,0x73,0x74,0x75,0x76,0x77,0x78,0x79,0x7A,0x2E,0x2E,0x2E,0x2E,0x2E
 
-	exit_msg: db 10," ..all finished!!",10								;exit message
-	exit_msg_len: equ $-exit_msg									;exit message length
+	exit_msg: db 10," ..all finished!!",10				;exit message
+	exit_msg_len: equ $-exit_msg					;exit message length
 
 section .bss
-	WIDTH equ 16											;width of output
-	buffer: resb WIDTH										;buffer for reading data
-	buffer_len: equ $-buffer									;length of input buffer
+	WIDTH equ 16							;width of output
+	buffer: resb WIDTH						;buffer for reading data
+	buffer_len: equ $-buffer					;length of input buffer
 
 section .text
 
-
-ClearLines:												;function to clear output buffer
+ClearLines:								;function to clear output buffer
 
 ;INPUT:nothing
 ;OUTPUT:nothing
@@ -36,16 +35,16 @@ ClearLines:												;function to clear output buffer
 	push ecx
 	push eax
 	xor eax,eax
-	mov ecx,WIDTH-1											;setting counter for clearing loop
-	mov al,0x30											;move dot in al register
+	mov ecx,WIDTH-1							;setting counter for clearing loop
+	mov al,0x30							;move dot in al register
 	.start_loop:
-		call DumpChar										;call function to insert char
-		dec ecx											;decrement loop counter
-		jnz .start_loop										;start another loop cycle
-	pop eax												;restore affected registers
+		call DumpChar						;call function to insert char
+		dec ecx							;decrement loop counter
+		jnz .start_loop						;start another loop cycle
+	pop eax								;restore affected registers
 	pop ecx
 	ret
-DumpChar:												;function to insert single character/byte to output buffer (both hex digit and ascii)
+DumpChar:								;function to insert single character/byte to output buffer (both hex digit and ascii)
 
 ;INPUT: ecx:position of char tu dump, eax/al:byte to dump
 ;OUTPUT: nothing
@@ -56,21 +55,21 @@ DumpChar:												;function to insert single character/byte to output buffer 
 	push edx
 	xor ebx,ebx
 	xor edx,edx
-	mov bl,al;											;duplicate the input value
-	shr al,0x4											;shift the high nibble
-	mov dl,byte [hxdigit+eax]									;lookup the nibble in hex digit lkp table
-	mov byte [hxdump_line+ecx*3],dl									;move the digit to output buffer
-	mov al,bl											;restore the al register from bl
-	and al,0x0f											;mask out the low nibble
-	mov dl,byte [hxdigit+eax]									;lookup the low nibble in the hexdigit lkp table
-	mov byte [hxdump_line+ecx*3+1],dl								;store the looked up hex digit in the outputbuffer
-	mov dl,byte [ascii+ebx]										;lookup the ascii value in ascii table for the dumped char/byte
-	mov byte [ascii_line+ecx+1],dl									;store the ascii value in the output buffer
-	pop edx												;restore registers
+	mov bl,al;							;duplicate the input value
+	shr al,0x4							;shift the high nibble
+	mov dl,byte [hxdigit+eax]					;lookup the nibble in hex digit lkp table
+	mov byte [hxdump_line+ecx*3],dl					;move the digit to output buffer
+	mov al,bl							;restore the al register from bl
+	and al,0x0f							;mask out the low nibble
+	mov dl,byte [hxdigit+eax]					;lookup the low nibble in the hexdigit lkp table
+	mov byte [hxdump_line+ecx*3+1],dl				;store the looked up hex digit in the outputbuffer
+	mov dl,byte [ascii+ebx]						;lookup the ascii value in ascii table for the dumped char/byte
+	mov byte [ascii_line+ecx+1],dl					;store the ascii value in the output buffer
+	pop edx								;restore registers
 	pop ebx
 	pop eax
 	ret
-PrintLine:												;function to printout the outputbuffer 
+PrintLine:								;function to printout the outputbuffer 
 
 ;INPUT:nothing
 ;OUTPUT:nothing
@@ -85,7 +84,7 @@ PrintLine:												;function to printout the outputbuffer
 	popad
 	ret
 
-LoadBuffer:												;function to read the string into input buffer
+LoadBuffer:								;function to read the string into input buffer
 
 ;INPUT:nothing
 ;OUTPUT:eax:number of chars read
@@ -99,8 +98,8 @@ LoadBuffer:												;function to read the string into input buffer
 	mov ecx,buffer
 	mov edx,buffer_len
 	int 0x80
-	cmp eax,0x0											;check if we reached end of file
-	jz Ex												;if yes, then finish the program
+	cmp eax,0x0							;check if we reached end of file
+	jz Ex								;if yes, then finish the program
 	pop edx
 	pop ecx
 	pop ebx
@@ -109,21 +108,21 @@ LoadBuffer:												;function to read the string into input buffer
 global _start
 
 _start:
-	.begin:												;begin reading new buffer
-	call ClearLines											;clear out output buffers
-	call LoadBuffer											;read string to input buffer
-	mov edi,eax											;eax returns nr of bytes read from load
-	.scan:												;scan the loaded buffer
-		movzx eax,byte [buffer+edi-1]								;move 1 read byte to eax
-		mov ecx,edi										;edi stores the decrementing counter, replicate it to ecx
+	.begin:								;begin reading new buffer
+	call ClearLines							;clear out output buffers
+	call LoadBuffer							;read string to input buffer
+	mov edi,eax							;eax returns nr of bytes read from load
+	.scan:								;scan the loaded buffer
+		movzx eax,byte [buffer+edi-1]				;move 1 read byte to eax
+		mov ecx,edi						;edi stores the decrementing counter, replicate it to ecx
 		dec ecx
-		call DumpChar										;dump the char into output buffers
-		dec edi											;decrement counter
-		jnz .scan										;check if we reached end of buffer
-	call PrintLine											;when at the end of buffer print the outputbuffer to output
-	jmp .begin											;go back to reading a new input buffer
+		call DumpChar						;dump the char into output buffers
+		dec edi							;decrement counter
+		jnz .scan						;check if we reached end of buffer
+	call PrintLine							;when at the end of buffer print the outputbuffer to output
+	jmp .begin							;go back to reading a new input buffer
 
-Ex:													;exit: write out exit message and return
+Ex:									;exit: write out exit message and return
 
 	mov eax,0x4
 	mov ebx,0x1
